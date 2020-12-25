4b99b9fd028eeed729b4f7c6816e25f41697a01a
theskymaks <theskymaks@gmail.com>
2020-12-25 03:16:50 +0200

    0.0.0.6 ----------------------------- done mvp of css file

--- Models/Arts/IArts.cs
+++ Models/Arts/IArts.cs
@@ -3,6 +3,8 @@
     interface IArts
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public string LinkOfArt { get; set; }
     }
 }
